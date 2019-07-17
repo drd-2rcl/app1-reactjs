88abad04ff596cc271b98b7973350cb08640bc31
Diogo <drd.rclrd@gmail.com>
2019-07-16 23:13:32 -0300

    was added methods for handle and insert new data

--- src/components/TechList.js
+++ src/components/TechList.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react'
 
 class TechList extends Component {
   state = {
+    newTech: '',
     techs: [
       'NodeJs',
       'ReactJs',
@@ -9,16 +10,25 @@ class TechList extends Component {
     ]
   }
 
+  handleInputChange = e => this.setState({ newTech: e.target.value });
+
+  handleSubmit = e => {
+    e.preventDefault();
+    this.setState({ techs: [ ...this.state.techs, this.state.newTech ], newTech: '' })
+  }
+
   render() {
-    console.log(this.state)
+    const { techs, newTech } = this.state;
+
     return (
-      <div>
+      <>
+      <h1>{newTech}</h1>
         <ul>
-          <li>NodeJs</li>
-          <li>ReactJs</li>
-          <li>React Native</li>
+          {techs.map(tech => <li key={tech}>{tech}</li>)}
         </ul>
-      </div>
+        <input type="text" onChange={this.handleInputChange} value={newTech}/>
+        <button onClick={this.handleSubmit}>Enviar</button>
+      </>
     )
   }
 }
